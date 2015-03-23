642d6e6e27fb487eb7a0902017fd93cf71cb56a0
Chris Mather <mather.chris@gmail.com>
2015-03-23 16:32:53 -0700

    add creating components code

--- app/app.jsx
+++ app/app.jsx
@@ -1,1 +1,36 @@
-console.log('hello world');
+class CommentList extends React.Component {
+  render() {
+    return (
+      <div className="comment-list">
+        CommentList
+      </div>
+    );
+  }
+}
+
+class CommentForm extends React.Component {
+  render() {
+    return (
+      <div className="comment-form">
+        CommentForm
+      </div>
+    );
+  }
+}
+
+class CommentBox extends React.Component {
+  render() {
+    return (
+      <div className="comment-box">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+}
+
+React.render(
+  <CommentBox/>,
+  document.getElementById('content')
+);
