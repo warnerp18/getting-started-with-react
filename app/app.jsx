d37bb17d564b4a543fc6c34f9acef0da19869cce
Chris Mather <mather.chris@gmail.com>
2015-03-23 17:07:51 -0700

    add component properties

--- app/app.jsx
+++ app/app.jsx
@@ -1,8 +1,29 @@
+class Comment extends React.Component {
+  render() {
+    return (
+      <div className="comment">
+        <div className="comment-body">
+          {this.props.children}
+        </div>
+        <div className="comment-author">
+          {this.props.author}
+        </div>
+      </div>
+    );
+  }
+}
+
 class CommentList extends React.Component {
   render() {
     return (
       <div className="comment-list">
-        CommentList
+        <Comment author="Chris Mather">
+          This is the first comment
+        </Comment>
+
+        <Comment author="Another Guy">
+          Some other text
+        </Comment>
       </div>
     );
   }
